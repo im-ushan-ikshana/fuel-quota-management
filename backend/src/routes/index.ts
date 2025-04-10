21f3fd5910d189744da07b592c4d0bbdd949bc00
Ushan Ikshana <ikushan23261@gmail.com>
2025-04-10 15:34:11 +0530

    refactor: reorganize and enhance comments in index.ts for clarity

--- backend/src/routes/index.ts
+++ backend/src/routes/index.ts
@@ -1,9 +1,16 @@
-import { Router } from 'express';
-import authRoutes from './auth.routes';
-import userRoutes from './user.routes';
-import vehicleRoutes from './vehicle.routes';
-import stationRoutes from './station.routes';
+/**
+ * @file index.ts
+ * @description Main router file for the Express server. -- main router file
+ * @author @im-ushan-ikshana
+ */
 
+import { Router } from 'express';                       //import router from express
+import authRoutes from './auth.routes';                 // import auth routes
+import userRoutes from './user.routes';                 // import user routes
+import vehicleRoutes from './vehicle.routes';           // import vehicle routes
+import stationRoutes from './station.routes';           // import station routes
+
+//import the express router -- built in functionality of express to create a router object
 const router = Router();
 
 //Add all routes here - this is the main router
@@ -12,4 +19,5 @@ router.use('/users', userRoutes);
 router.use('/vehicles', vehicleRoutes);
 router.use('/stations', stationRoutes);
 
+//export the router for use in other files -- handles as a module (can be used in server and test files)
 export default router;
