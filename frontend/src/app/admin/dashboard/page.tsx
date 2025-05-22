07154555804af7eff7b784f5f883327af1e3b2ba
Chathumi  Perera <chathumiamarasinghe@gmail.com>
2025-05-23 00:57:49 +0530

    create dashbord design

---
+++ frontend/src/app/admin/dashboard/page.tsx
@@ -0,0 +1,10 @@
+import Layout from "@/components/Layout";
+
+export default function DashboardPage() {
+  return (
+    <Layout>
+      <h2 className="text-2xl font-bold mb-4">Welcome to Admin Dashboard</h2>
+      <p>This is your overview page.</p>
+    </Layout>
+  );
+}
